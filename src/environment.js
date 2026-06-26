f7f5587a6dcaab5e6fa8d7ddf3c4d9be8422675e
Akif <akif@Mac.lan>
2026-06-26 12:01:06 +0530

    fix to backend endpoint

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ? "http://ec2-54-206-26-163.ap-southeast-2.compute.amazonaws.com:5000"
+  ? "https://syncmeet-akif.duckdns.org"
   : "http://localhost:5000";
 
 export default server;
